4e566d99e57d5181a0a3694295852665797756ce
Aimen <134489692+AimenNadeem@users.noreply.github.com>
2024-12-13 13:28:10 +0500

    Create ParkersDataI.ts

---
+++ src/assets/carsData/ParkersDataI.ts
@@ -0,0 +1,11 @@
+export interface ParkersDataI {
+    
+    employeeId: number,
+    employeeName: string,
+    vehicleType: string,
+    vehicleIdentity: string,
+    parkingName: string,
+    parkingSpace: number,
+    parkingLocation: string,
+    registeredDate: string,
+}
