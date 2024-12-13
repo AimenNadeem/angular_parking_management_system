f02ec0df5b60ec779d3f4a5dda994746fdd10735
Aimen <134489692+AimenNadeem@users.noreply.github.com>
2024-12-13 13:03:29 +0500

    Create parking.component.ts

---
+++ src/app/parking/parking.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parking',
+  templateUrl: './parking.component.html',
+  styleUrls: ['./parking.component.css']
+})
+export class ParkingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+}
