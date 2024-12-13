02486117152e6079a892e0f3090f8783b7fe54f5
Aimen <134489692+AimenNadeem@users.noreply.github.com>
2024-12-13 12:48:09 +0500

    Create car-box.component.spec.ts

---
+++ src/app/car-box/car-box.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CarBoxComponent } from './car-box.component';
+
+describe('CarBoxComponent', () => {
+  let component: CarBoxComponent;
+  let fixture: ComponentFixture<CarBoxComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CarBoxComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CarBoxComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
