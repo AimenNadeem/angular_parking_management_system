a6a66e19d4ef819142c0daa2a1912645b383461c
Aimen <134489692+AimenNadeem@users.noreply.github.com>
2024-12-13 13:50:34 +0500

    Create UserDataI.ts

---
+++ src/assets/userData/UserDataI.ts
@@ -0,0 +1,14 @@
+export interface UserDataI {
+    
+    id: number,
+    employeeId : number,
+    employeeName : string,
+    mobileNo : string,
+    email : string,
+    accountNo : string,
+    userName : string,
+    password : string,
+    isAdmin : boolean,
+    rank : number,
+    rankVal : null | any,
+};
