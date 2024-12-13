87e2d36a5cf51956ed55f2c66975eddb02a15fb3
Aimen <134489692+AimenNadeem@users.noreply.github.com>
2024-12-13 13:31:04 +0500

    Create extra_data.ts

---
+++ src/assets/dummyData/extraInfoData/extra_data.ts
@@ -0,0 +1,39 @@
+export const extraData = [
+
+    {
+        id: 0,
+        workPlace: "Bank Alfalah",
+        job: "developer",
+        cellNo: "000-000-000",
+    },
+    {
+        id: 1,
+        workPlace: "Bank Alfalah",
+        job: "accountant",
+        cellNo: "000-000-000",
+    },
+    {
+        id: 2,
+        workPlace: "Bank Alfalah",
+        job: "laywer",
+        cellNo: "000-000-000",
+    },
+    {
+        id: 3,
+        workPlace: "Bank Alfalah",
+        job: "developer",
+        cellNo: "000-000-000",
+    },
+    {
+        id: 4,
+        workPlace: "Bank Alfalah",
+        job: "manager",
+        cellNo: "000-000-000",
+    },
+    {
+        id: 5,
+        workPlace: "Bank Alfalah",
+        job: "ceo",
+        cellNo: "000-000-000",
+    },
+];
