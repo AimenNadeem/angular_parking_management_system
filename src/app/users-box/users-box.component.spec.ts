fad989dfd96bcc112aef5b3abbd4f6ca0e3a556f
Aimen <134489692+AimenNadeem@users.noreply.github.com>
2024-12-13 13:24:52 +0500

    Create users-box.component.spec.ts

---
+++ src/app/users-box/users-box.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UsersBoxComponent } from './users-box.component';
+
+describe('UsersBoxComponent', () => {
+  let component: UsersBoxComponent;
+  let fixture: ComponentFixture<UsersBoxComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UsersBoxComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UsersBoxComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
